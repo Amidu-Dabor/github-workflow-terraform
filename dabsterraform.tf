7dd45956d98236d0dfab3e0f30c89f24e87b162b
Amidu Dabor <36164952+Amidu-Dabor@users.noreply.github.com>
2024-06-27 03:59:10 +0000

    Adding Public Subnet & Availability Zones Config to Terraform Template

--- dabsterraform.tf
+++ dabsterraform.tf
@@ -4,8 +4,9 @@ provider "aws" {
 }
 
 module "dabsvpc" {
-    source = "terraform-aws-modules/vpc/aws"    cidr = "10.7.0.0/16"
+  source = "terraform-aws-modules/vpc/aws"
+  cidr = "10.7.0.0/16"
 
-    azs             = ["us-east-1a", "us-east-1b"]
-    public_subnets  = ["10.7.100.0/24", "10.7.200.0/24"]
+  azs = ["us-east-1a", "us-east-1b"]
+  public_subnets = ["10.7.100.0/24", "10.7.200.0/24"]
 }
